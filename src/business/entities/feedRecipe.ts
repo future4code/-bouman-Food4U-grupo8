7cfd3a8af9db266c41874a4b2d79546c71f67a4d
Patricia <patymatiesque@gmail.com>
2020-03-18 11:48:04 -0300

    FeedRecipe, FeedGateway, GetFeed e FeedDataBase criados

---
+++ src/business/entities/feedRecipe.ts
@@ -0,0 +1,18 @@
+import { Recipe } from "./recipe";
+
+export class FeedRecipe extends Recipe{
+    constructor(
+        id: string,
+        title: string,
+        description: string,
+        creationDate: Date,
+        userId: string,
+        private userEmail: string
+    ){
+        super(id, title, creationDate, description, userId)
+    }
+
+    getUserEmail(){
+        return this.userEmail
+    }
+}
---
+++ src/business/usecase/users/getFeed.ts
@@ -0,0 +1,35 @@
+import { FeedGateway } from "../../gateways/feedGateway";
+
+
+
+export class GetFeefUC {
+    constructor(private feedGateway: FeedGateway) { }
+
+    async execute(input: GetFeedInput): Promise<GetFeedOutput[]> {
+        const recipes = await this.feedGateway.getFeedForUser(input.userId)
+
+        return recipes.map(recipe => {
+            return {
+            recipeId: recipe.getId(),
+            title: recipe.getTitle(),
+            description: recipe.getDescription(),
+            creationDate: recipe.getCreationDate(),
+            userId: recipe.getUserId(),
+            userEmail: recipe.getUserEmail(),
+           }
+        })
+
+    }
+
+}
+
+export interface GetFeedInput {
+    recipeId: string
+    title: string
+    description: string
+    creationDate: number
+}
+export interface GetFeedOutput {
+    userId: string
+}
+
